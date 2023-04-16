6e5c43080813473ea052bb162a7529f532c18217
Bruno Luiz <brunolts15@gmail.com>
2023-04-16 11:28:59 -0300

    just making it more readable

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func setupRoutes(app *fiber.App) {
 	app.Get("/api/users", routes.GetUsers)
 	app.Get("/api/users/:id", routes.GetUser)
 	app.Put("/api/users/:id", routes.UpdateUser)
-	app.Delete("api/users/:id", routes.DeleteUser)
+	app.Delete("/api/users/:id", routes.DeleteUser)
 
 	// Product endpoint
 	app.Post("/api/products", routes.CreateProduct)
